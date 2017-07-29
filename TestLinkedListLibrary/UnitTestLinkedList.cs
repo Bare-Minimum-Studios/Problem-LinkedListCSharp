413ad3eef53292a5da463ec78a7b7d87a0fa09be
jruiz30 <jruiz30@gatech.edu>
2017-07-28 20:29:54 -0400

    Renamed unit test case

--- TestLinkedListLibrary/UnitTestLinkedList.cs
+++ TestLinkedListLibrary/UnitTestLinkedList.cs
@@ -7,7 +7,7 @@ namespace TestLinkedListLibrary
     public class UnitTestLinkedList
     {
         [Fact]
-        public void Test1()
+        public void CountTest01()
         {
             var myLinkedList = new LinkedList<string>();
             Assert.Equal(0,myLinkedList.Count); 
